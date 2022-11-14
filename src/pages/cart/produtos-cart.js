c738091a9fe29adf0d5c04f327780a0e78c826c0
Souto-Pedro <pedro.souto@outlook.com.br>
2022-11-13 21:09:12 -0300

    final commit

--- src/pages/cart/produtos-cart.js
+++ src/pages/cart/produtos-cart.js
@@ -1,49 +1,54 @@
 const productsCart = {
-  33: {
-    name: "Produto 3",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus lacus vel lectus dictum aliquam. In laoreet, arcu in porttitor semper, magna diam consectetur arcu, vel condimentum risus erat vel lacus. Duis ut viverra est, a facilisis augue. Integer feugiat risus risus. Curabitur facilisis ipsum at libero ullamcorper, id porta sapien rutrum. Integer egestas egestas quam, non venenatis quam lacinia sit amet. Ut massa metus, volutpat sit amet feugiat dapibus, tincidunt non est. Cras pharetra ut massa eu porta. Phasellus velit magna, pretium eleifend turpis in, dictum vehicula nunc.",
-    images: [
-      "https://static3.tcdn.com.br/img/img_prod/769840/vestido_mirele_1329_1_20348177a5ec7e4dc844aae226aac5df.jpg",
-      "https://static3.tcdn.com.br/img/img_prod/769840/vestido_mirele_1329_1_20348177a5ec7e4dc844aae226aac5df.jpg",
-      "https://static3.tcdn.com.br/img/img_prod/769840/vestido_mirele_1329_1_20348177a5ec7e4dc844aae226aac5df.jpg",
-    ],
-    price: 299.99,
-    categories: [1, 2, 3],
-    quantity: 3,
-  },
+
   1: {
-    name: "Vestido laranja",
+    name: "Spacex Hoodie",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus lacus vel lectus dictum aliquam. In laoreet, arcu in porttitor semper, magna diam consectetur arcu, vel condimentum risus erat vel lacus. Duis ut viverra est, a facilisis augue. Integer feugiat risus risus. Curabitur facilisis ipsum at libero ullamcorper, id porta sapien rutrum. Integer egestas egestas quam, non venenatis quam lacinia sit amet. Ut massa metus, volutpat sit amet feugiat dapibus, tincidunt non est. Cras pharetra ut massa eu porta. Phasellus velit magna, pretium eleifend turpis in, dictum vehicula nunc.",
     images: [
-      "https://static3.tcdn.com.br/img/img_prod/769840/vestido_mirele_1329_1_20348177a5ec7e4dc844aae226aac5df.jpg",
-      "https://ph-cdn3.ecosweb.com.br/imagens01/foto/moda-feminina/vestido-midi/vestido-laranja-neon-com-babado-no-decote_337997_600_1.jpg",
-      "https://secretocloset.com.br/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/v/c/vc61.jpg",
+      "https://d3ugyf2ht6aenh.cloudfront.net/stores/726/919/products/mockup-of-a-customizable-crewneck-sweatshirt-hanging-against-a-concrete-wall-339971-d6795c1805d90d4a0915908462359356-640-0.png",
+      "https://d3ugyf2ht6aenh.cloudfront.net/stores/726/919/products/mescla-spaceex1-5b842b23f17cf0b5e515908462346917-640-0.jpg",
+      "https://d3ugyf2ht6aenh.cloudfront.net/stores/726/919/products/mockup-of-a-customizable-crewneck-sweatshirt-hanging-against-a-concrete-wall-339971-d6795c1805d90d4a0915908462359356-640-0.png",
+    ],
+    price: 399.99,
+    promo_price: 249.99,
+    percent: 15,
+    categories: [1, 3],
+    quantity: 2,
+  },
+  4: {
+    name: "T-shirt: My next destination: Moon",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus lacus vel lectus dictum aliquam. In laoreet, arcu in porttitor semper, magna diam consectetur arcu, vel condimentum risus erat vel lacus. Duis ut viverra est, a facilisis augue. Integer feugiat risus risus. Curabitur facilisis ipsum at libero ullamcorper, id porta sapien rutrum. Integer egestas egestas quam, non venenatis quam lacinia sit amet. Ut massa metus, volutpat sit amet feugiat dapibus, tincidunt non est. Cras pharetra ut massa eu porta. Phasellus velit magna, pretium eleifend turpis in, dictum vehicula nunc.",
+    images: [
+      "https://d3ugyf2ht6aenh.cloudfront.net/stores/726/919/products/my-next-destination_-moon-p1-5dde86367dde547e2315865609760538-640-0.png",
+      "https://d3ugyf2ht6aenh.cloudfront.net/stores/726/919/products/my-next-destination_-moon-bordo1-e7b2b57b0e88806da215865609762666-640-0.png",
+      "https://d3ugyf2ht6aenh.cloudfront.net/stores/726/919/products/my-next-destination_-moon-a1-5fde45af3e97d66f1c15865609762276-640-0.png",
+    ],
+    price: 99.99,
+    promo_price:59.99,
+    percent:40,
+    categories: [1],
+    quantity: 2,
+  },
+  8: {
+    name: "Hoodie Property of NASA",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus lacus vel lectus dictum aliquam. In laoreet, arcu in porttitor semper, magna diam consectetur arcu, vel condimentum risus erat vel lacus. Duis ut viverra est, a facilisis augue. Integer feugiat risus risus. Curabitur facilisis ipsum at libero ullamcorper, id porta sapien rutrum. Integer egestas egestas quam, non venenatis quam lacinia sit amet. Ut massa metus, volutpat sit amet feugiat dapibus, tincidunt non est. Cras pharetra ut massa eu porta. Phasellus velit magna, pretium eleifend turpis in, dictum vehicula nunc.",
+    images: [
+      "https://d3ugyf2ht6aenh.cloudfront.net/stores/726/919/products/cinza-mescla71-522f876cb60e38181d16137454234071-640-0.png",
+      "https://d3ugyf2ht6aenh.cloudfront.net/stores/726/919/products/branca401-9114a655a794eafa0216137454234298-640-0.png",
+      "https://d3ugyf2ht6aenh.cloudfront.net/stores/726/919/products/cinza-mescla71-522f876cb60e38181d16137454234071-640-0.png",
     ],
     price: 299.99,
     promo_price: 199.99,
     percent: 15,
-    categories: [1, 2],
-    quantity: 2,
+    categories: [1, 3],
+    quantity: 1,
   },
-  2: {
-    name: "Produto 2",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus lacus vel lectus dictum aliquam. In laoreet, arcu in porttitor semper, magna diam consectetur arcu, vel condimentum risus erat vel lacus. Duis ut viverra est, a facilisis augue. Integer feugiat risus risus. Curabitur facilisis ipsum at libero ullamcorper, id porta sapien rutrum. Integer egestas egestas quam, non venenatis quam lacinia sit amet. Ut massa metus, volutpat sit amet feugiat dapibus, tincidunt non est. Cras pharetra ut massa eu porta. Phasellus velit magna, pretium eleifend turpis in, dictum vehicula nunc.",
-    images: [
-      "https://static3.tcdn.com.br/img/img_prod/769840/vestido_mirele_1329_1_20348177a5ec7e4dc844aae226aac5df.jpg",
-      "https://static3.tcdn.com.br/img/img_prod/769840/vestido_mirele_1329_1_20348177a5ec7e4dc844aae226aac5df.jpg",
-      "https://static3.tcdn.com.br/img/img_prod/769840/vestido_mirele_1329_1_20348177a5ec7e4dc844aae226aac5df.jpg",
-    ],
-    price: 299.99,
-    promo_price: 199.99,
-    percent: 15,
-    categories: [1, 3, 4],
-    quantity: 2,
-  },
+
 };
 
 module.exports = {
   productsCart,
 };
+
